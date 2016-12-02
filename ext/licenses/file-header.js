b170a59ee40bd62b8d22eb3bb859e33dd9532dff
Lee Boonstra <lee.boonstra@gmail.com>
2016-12-02 09:32:05 +0100

    framework

---
+++ ext/licenses/file-header.js
@@ -0,0 +1,18 @@
+/*
+This file is part of Ext JS 6.2.1.124
+
+Copyright (c) 2011-2016 Sencha Inc
+
+Contact:  http://www.sencha.com/contact
+
+Pre-release code in the Ext repository is intended for development purposes only and will
+not always be stable. 
+
+Use of pre-release code is permitted with your application at your own risk under standard
+Ext license terms. Public redistribution is prohibited.
+
+For early licensing, please contact us at licensing@sencha.com
+
+Version: 6.2.1.124 Build date: 2016-11-01 21:12:13 (00d72a80f147b631640ef19a0325ce8785b015aa)
+
+*/
